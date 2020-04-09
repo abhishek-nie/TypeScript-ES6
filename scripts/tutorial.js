4a118d5f47917f27631142c9395326fa41e1f170
GLOBAL\I337265 <abhishek.kumar20@sap.com>
2020-04-09 22:06:03 +0530

    [typescript setup]

---
+++ scripts/tutorial.js
@@ -0,0 +1,12 @@
+"use strict";
+function greetPerson(name) {
+    if (name === "AK") {
+        var greet = "Hello AK";
+    }
+    else {
+        var greet = "HI there";
+    }
+    console.log(greet);
+}
+greetPerson("AK");
+//# sourceMappingURL=tutorial.js.map
---
+++ tutorial.ts
@@ -0,0 +1,11 @@
+function greetPerson(name){
+    if(name === "AK"){
+        var greet ="Hello AK"
+    }
+    else{
+       var greet="HI there"
+    }
+    console.log(greet);
+
+}
+greetPerson("AK")
